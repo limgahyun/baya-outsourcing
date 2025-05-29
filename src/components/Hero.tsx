9e055a3fde84c52b4976fba1f303cc635c8157b1
limgahyun <limgh2002@gmail.com>
2025-05-29 18:03:53 +0900

    design: hero section content 가운데 정렬

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,8 +20,8 @@ export default function Hero() {
       {/* Content */}
       <div className="relative z-20 h-full flex items-center">
         <div className="w-full md:w-1/2 md:ml-auto">
-          <div className="max-w-xl mx-auto px-4 text-center md:text-left text-white break-keep">
-            <h1 className="font-gmarket font-medium mb-6">
+          <div className="max-w-xl mx-auto px-4 text-center md:text-left text-white break-keep flex flex-col items-center">
+            <h1 className="font-gmarket font-medium mb-6 text-center">
               <div className="text-2xl md:text-3xl lg:text-4xl mb-2 break-keep">
                 사업을 이해하는 개발 파트너,
               </div>
@@ -29,11 +29,11 @@ export default function Hero() {
                 <span className="text-blue-300">뚝딱랩</span> 입니다
               </div>
             </h1>
-            <p className="font-pretendard text-lg md:text-xl lg:text-2xl mb-12 whitespace-pre-line text-gray-100 break-keep">
-              {
-                "개발자가 아닌 사업의 본질을 꿰뚫는 파트너\n2분만 둘러보세요\n왜 뚝딱랩이 해답인지 아실겁니다."
-              }
-            </p>
+            <div className="font-pretendard text-lg md:text-xl lg:text-2xl mb-12 whitespace-pre-line text-gray-100 break-keep text-center">
+              <p>개발자가 아닌 사업의 본질을 꿰뚫는 파트너</p>
+              <p>2분만 둘러보세요.</p>
+              <p>왜 뚝딱랩이 해답인지 아실겁니다.</p>
+            </div>
             <button
               onClick={() => {
                 window.open("https://tally.so/r/mVyWdy", "_blank");
